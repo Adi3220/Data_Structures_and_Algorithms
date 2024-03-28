6eb0df048ebc3e376bb46fb2ab172b642fcb42c5
Aditya Damare <117853273+Adi3220@users.noreply.github.com>
2024-03-28 23:18:31 +0530

    Create  Find Area of Rectangle.cpp
    Problem statement:
    Take the length(L) and breadth(B) of the rectangle as input and find its area.
    
    Note:
    Length and breadth must be an integer value and the area will always be in the range of integers.

---
+++ Find Area of Rectangle.cpp
@@ -0,0 +1,14 @@
+// Find Area of Rectangle
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    //Write your code here
+    int length,breadth;
+    cin>>length;
+    cin>>breadth;
+    cout<<length*breadth;
+
+    return 0;
+}
