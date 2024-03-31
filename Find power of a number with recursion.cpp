da009416f8ad3c28c7e02ea1845dfc5223ce2b30
Aditya Damare <117853273+Adi3220@users.noreply.github.com>
2024-03-31 10:55:42 +0530

    Create  Find power of a number with recursion.cpp
    Problem statement:
    Ninja is sitting in an examination hall. He is encountered with a problem statement, "Find ‘X’ to the power ‘N’ (i.e. ‘X’ ^ ‘N’). Where ‘X’ and ‘N’ are two integers."
    
    Ninja was not prepared for this question at all, as this question was unexpected in the exam.
    
    He is asking for your help to solve this problem. Help Ninja to find the answer to the problem.
    
    Note :
    
    For this question, you can assume that 0 raised to the power of 0 is 1.

---
+++ Find power of a number with recursion.cpp
@@ -0,0 +1,11 @@
+// Find power of a number
+
+#include <bits/stdc++.h> 
+long long Pow(int X, int N)
+{
+    // Write your code here. 
+    if(N == 0){
+        return 1;
+    }
+    return X*Pow(X, N-1);
+}
