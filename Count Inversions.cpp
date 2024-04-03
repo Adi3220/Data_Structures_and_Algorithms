f97867ffa474e14b501070befeecc7e2700cbed5
Aditya Damare <117853273+Adi3220@users.noreply.github.com>
2024-04-03 14:23:14 +0530

    Create  Count Inversions.cpp
    Problem statement:
    For a given integer array/list 'ARR' of size 'N' containing all distinct values, find the total number of 'Inversions' that may exist.
    
    An inversion is defined for a pair of integers in the array/list when the following two conditions are met.
    
    A pair ('ARR[i]', 'ARR[j]') is said to be an inversion when:
    
    1. 'ARR[i] > 'ARR[j]' 
    2. 'i' < 'j'
    
    Where 'i' and 'j' denote the indices ranging from [0, 'N').

---
+++ Count Inversions.cpp
@@ -0,0 +1,15 @@
+// Count Inversions
+
+#include <bits/stdc++.h> 
+long long getInversions(long long *arr, int n){
+    // Write your code here.
+    int count=0;
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            if(arr[i]>arr[j] && i<j){
+                count++;
+            }
+        }
+    }
+    return count;
+}
