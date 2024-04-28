4f0cdfd86dfe6d199b6cad9822e24557b2d374de
Aditya Damare <117853273+Adi3220@users.noreply.github.com>
2024-04-28 11:40:59 +0530

    Create  Reverse Linked List.cpp
    Problem statement:
    You are given a Singly Linked List of integers. You need to reverse the Linked List by changing the links between nodes.
    
    
    
    Example:
    
    Input:
    2 4 5 -1
    
    Output:
    5 4 2 -1
    
    Explanation: 2->4->5 is the initial linked list. If we reverse this, we get 5->4->2.

---
+++ Reverse Linked List.cpp
@@ -0,0 +1,45 @@
+// Reverse Linked List
+
+/****************************************************************
+
+    Following is the class structure of the Node class:
+
+        class Node
+        {
+        public:
+            int data;
+            Node *next;
+            Node()
+            {
+                this->data = 0;
+                next = NULL;
+            }
+            Node(int data)
+            {
+                this->data = data;
+                this->next = NULL;
+            }
+            Node(int data, Node* next)
+            {
+                this->data = data;
+                this->next = next;
+            }
+        };
+        
+
+*****************************************************************/
+
+Node* reverseLinkedList(Node *head)
+{
+    // Write your code here
+    Node* cur = head;
+    Node* prev = NULL;
+
+    while(cur != NULL){
+        Node* forward = cur->next;
+        cur->next = prev;
+        prev = cur;
+        cur = forward;
+    }
+    return prev;
+}
