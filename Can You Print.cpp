a8a39f6f031197948609dbbdf1fd0ae90d198cc8
Aditya Damare <117853273+Adi3220@users.noreply.github.com>
2024-07-13 11:44:08 +0530

    Create  Can You Print.cpp
    Problem statement:
    You are given an integer ‘N’. You have to print integers from 1 to N.
    
    For Example:
    Input: 'N' = 6
    
    Output: 1 2 3 4 5 6
    
    If 'N' = 6, The numbers from 1 to 6 are 1, 2, 3, 4, 5, 6.
    Hence, the output will be 1 2 3 4 5 6.

---
+++ Can You Print.cpp
@@ -0,0 +1,11 @@
+// Can You Print
+
+#include <bits/stdc++.h> 
+vector < int > canYouPrint(int n) {
+    // Write your code here.
+    vector<int> v;
+    for(int i=1; i<=n; i++){
+        v.push_back(i);
+    }
+    return v;
+}
