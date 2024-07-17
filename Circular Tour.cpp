13085ab3e9d6b79af96310a9d6a6c6be14f0e328
Aditya Damare <117853273+Adi3220@users.noreply.github.com>
2024-07-17 15:23:48 +0530

    Create  Circular Tour.cpp
    Problem statement:
    You have been given a circular path. There are N petrol pumps on this path that are numbered from 0 to N - 1 (Both inclusive). Each petrol pump has two values associated with it:
    
    1)The amount of petrol that is available at this particular petrol pump.
    
    2)The distance to reach the next petrol pump.
    
    You are on a truck having an empty tank of infinite capacity. You can start the tour from any of the petrol pumps. Your task is to calculate the first petrol pump from where the truck will be able to complete the full circle or determine if it is impossible to do so.
    
    You may assume that the truck will stop at every petrol pump and it will add the petrol from that pump to its tank. The truck will move one kilometre for each litre of petrol consumed.

---
+++ Circular Tour.cpp
@@ -0,0 +1,24 @@
+// Circular Tour
+
+#include <bits/stdc++.h> 
+int firstCircularTour(vector<int>& petrol, vector<int>& distance, int N)
+{
+	// write your code here
+	long long int deficit=0;
+	long long int start=0;
+	long long int balance=0;
+	
+	for(int i=0; i<N; i++){
+		balance += petrol[i] - distance[i];
+		
+		if(balance < 0){
+			start = i+1;
+			deficit += balance;
+			balance = 0;
+		}
+	}
+	if((deficit + balance) >= 0){
+		return start;
+	}
+	return -1;
+}
