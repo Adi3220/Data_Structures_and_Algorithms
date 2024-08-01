870f142b894eb918a076b4a101c7c331b47d3c80
Aditya Damare <117853273+Adi3220@users.noreply.github.com>
2024-08-02 00:52:28 +0530

    Create  Postorder Binary Tree.cpp
    Problem statement:
    For a given Binary Tree of integers, print the post-order traversal.
    
    
    
    The postorder traversal for the given binary tree will be 40 50 20 30 10

---
+++ Postorder Binary Tree.cpp
@@ -0,0 +1,31 @@
+// Postorder Binary Tree
+
+/**********************************************************
+	Following is the Binary Tree Node class structure
+
+	template <typename T>
+	class BinaryTreeNode {
+    	public : 
+    	T data;
+    	BinaryTreeNode<T> *left;
+    	BinaryTreeNode<T> *right;
+
+    	BinaryTreeNode(T data) {
+        	this -> data = data;
+        	left = NULL;
+        	right = NULL;
+    	}
+	};
+
+***********************************************************/
+
+void postOrder(BinaryTreeNode<int> *root) {
+	// Write your code here
+	if(!root){
+		return;
+	}
+
+	postOrder(root->left);
+	postOrder(root->right);
+	cout<<root->data<<" ";
+}
