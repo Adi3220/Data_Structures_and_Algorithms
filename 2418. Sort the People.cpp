4b07b50c8a0d63512c3a5474f2d1f578f493e674
Aditya Damare <117853273+Adi3220@users.noreply.github.com>
2024-09-02 19:26:09 +0530

    Create 2418. Sort the People.cpp
    Problem Statement:
    You are given an array of strings names, and an array heights that consists of distinct positive integers. Both arrays are of length n.
    
    For each index i, names[i] and heights[i] denote the name and height of the ith person.
    
    Return names sorted in descending order by the people's heights.

---
+++ 2418. Sort the People.cpp
@@ -0,0 +1,17 @@
+//2418. Sort the People
+
+class Solution {
+public:
+    vector<string> sortPeople(vector<string>& names, vector<int>& heights) {
+        unordered_map<int, string> m;
+        int n = heights.size();
+        for(int i=0; i<n; i++){
+            m[heights[i]] = names[i];
+        }
+        sort(heights.rbegin(), heights.rend());
+        for(int i=0; i<n; i++){
+            names[i] = m[heights[i]];
+        }
+        return names;
+    }
+};
