941a619925c582fb599f247460b0662f33f99c5b
Aditya Damare <117853273+Adi3220@users.noreply.github.com>
2024-09-07 10:47:09 +0530

    Create  Validate BST.cpp
    Problem statement:
    You have been given a binary tree of integers with N number of nodes. Your task is to check if that input tree is a BST (Binary Search Tree) or not.
    
    A binary search tree (BST) is a binary tree data structure which has the following properties.
    
    • The left subtree of a node contains only nodes with data less than the node’s data.
    • The right subtree of a node contains only nodes with data greater than the node’s data.
    • Both the left and right subtrees must also be binary search trees.

---
+++ Validate BST.cpp
@@ -0,0 +1,54 @@
+// Validate BST
+
+#include <bits/stdc++.h> 
+/*************************************************************
+    Following is the Binary Tree node structure
+
+    template <typename T>
+    class BinaryTreeNode 
+    {
+      public : 
+        T data;
+        BinaryTreeNode<T> *left;
+        BinaryTreeNode<T> *right;
+
+        BinaryTreeNode(T data) 
+        {
+            this -> data = data;
+            left = NULL;
+            right = NULL;
+        }
+
+        ~BinaryTreeNode() 
+        {
+            if (left)
+            {
+                delete left;
+            }
+            if (right)
+            {
+                delete right;
+            }
+        }
+    };
+
+*************************************************************/
+bool isBST(BinaryTreeNode<int>* root, int min, int max){
+    if(!root){
+        return true;
+    }
+
+    if(root->data >= min && root->data < max){
+        bool l = isBST(root->left, min, root->data);
+        bool r = isBST(root->right, root->data, max);
+        return l && r;
+    }
+    else{
+        return false;
+    }
+}
+bool validateBST(BinaryTreeNode<int>* root) 
+{
+    // Write your code here
+    return isBST(root, INT_MIN, INT_MAX);
+}
